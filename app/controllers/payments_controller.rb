24baa18f1d5ceb54b8ce0e46b41945f1a1fccf41
Yung Chun Ern Martin <martinyce@gmial.com>
2016-08-10 15:14:38 +0800

    integrated braintree payment gateway

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,20 +1,33 @@
 class PaymentsController < ApplicationController
 	
 	def new
+		@payment = Payment.new
 		gon.client_token = generate_client_token
 	end
 
+	def create
+    @result = Braintree::Transaction.sale(
+              amount: total_price,
+              payment_method_nonce: params[:payment_method_nonce])
+    if @result.success?
+      current_user.payments.create(booking_id: @booking.id, paid: true, amount: total_price, transaction_id: @result.transaction.id)
+      redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
+    else
+    	
+      flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      gon.client_token = generate_client_token
+      render :new
+    end
+  end
 
 	private
-	def total_price
-		price_per_night = @listing.price
-		num_days = (@booking.start_date..@booking.end_date).count
-		return price_per_night * num_days
-	end
 
-	def set_listing
+	def total_price
 		@booking = Booking.find(params[:booking_id])
 		@listing = @booking.listing
+		price_per_night = @listing.price
+		num_days = (@booking.start_date..@booking.end_date).count - 1
+		return price_per_night * num_days
 	end
 
 	def generate_client_token
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810032502) do
+ActiveRecord::Schema.define(version: 20160810045315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,9 @@ ActiveRecord::Schema.define(version: 20160810032502) do
     t.integer  "booking_id"
     t.boolean  "paid"
     t.integer  "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "transaction_id"
   end
 
   create_table "tags", force: :cascade do |t|
